c979d3007b04ede03d133aebb40568ddb922cd53
Antonio Ruggiero <antonrugg@gmail.com>
2022-05-18 23:38:38 +0200

    styling changes

--- resources/views/comics.blade.php
+++ resources/views/comics.blade.php
@@ -5,7 +5,7 @@
 @section('content')
     @foreach($comics as $comic)
         <article>
-            <a href="#nowhere"><img src="{{ $comic['thumb'] }}" alt="{{ $comic['title'] }}"></a>
+            <a href="/comics/data"><img src="{{ $comic['thumb'] }}" alt="{{ $comic['title'] }}"></a>
             <a href="#nowhere"><p>{{ $comic['series'] }}</p></a>
         </article> 
     @endforeach
--- resources/views/data.blade.php
+++ resources/views/data.blade.php
@@ -3,7 +3,6 @@
 - Comics Info
 @endsection
 @section('content')
-    <h1>DATA SINGLE COMIC</h1>
-    <a href="/comics"><p>GO BACK</p></a>
+    
 @endsection
     
