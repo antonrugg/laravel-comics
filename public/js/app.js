a6b1586081b367dc2f2776840583fdc720961764
Antonio Ruggiero <antonrugg@gmail.com>
2022-05-19 08:38:45 +0200

    fixed comics/data style

--- public/js/app.js
+++ public/js/app.js
@@ -19425,15 +19425,63 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 /***/ }),
 
+/***/ "./resources/sass/comics.scss":
+/*!************************************!*\
+  !*** ./resources/sass/comics.scss ***!
+  \************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+// removed by extract-text-webpack-plugin
+
+/***/ }),
+
+/***/ "./resources/sass/data.scss":
+/*!**********************************!*\
+  !*** ./resources/sass/data.scss ***!
+  \**********************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+// removed by extract-text-webpack-plugin
+
+/***/ }),
+
+/***/ "./resources/sass/header.scss":
+/*!************************************!*\
+  !*** ./resources/sass/header.scss ***!
+  \************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+// removed by extract-text-webpack-plugin
+
+/***/ }),
+
+/***/ "./resources/sass/main.scss":
+/*!**********************************!*\
+  !*** ./resources/sass/main.scss ***!
+  \**********************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+// removed by extract-text-webpack-plugin
+
+/***/ }),
+
 /***/ 0:
-/*!*************************************************************!*\
-  !*** multi ./resources/js/app.js ./resources/sass/app.scss ***!
-  \*************************************************************/
+/*!*****************************************************************************************************************************************************************************!*\
+  !*** multi ./resources/js/app.js ./resources/sass/app.scss ./resources/sass/data.scss ./resources/sass/header.scss ./resources/sass/main.scss ./resources/sass/comics.scss ***!
+  \*****************************************************************************************************************************************************************************/
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
 __webpack_require__(/*! C:\Users\yomos\booleanprojects\phpboolean\laravel-comics\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Users\yomos\booleanprojects\phpboolean\laravel-comics\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\Users\yomos\booleanprojects\phpboolean\laravel-comics\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\Users\yomos\booleanprojects\phpboolean\laravel-comics\resources\sass\data.scss */"./resources/sass/data.scss");
+__webpack_require__(/*! C:\Users\yomos\booleanprojects\phpboolean\laravel-comics\resources\sass\header.scss */"./resources/sass/header.scss");
+__webpack_require__(/*! C:\Users\yomos\booleanprojects\phpboolean\laravel-comics\resources\sass\main.scss */"./resources/sass/main.scss");
+module.exports = __webpack_require__(/*! C:\Users\yomos\booleanprojects\phpboolean\laravel-comics\resources\sass\comics.scss */"./resources/sass/comics.scss");
 
 
 /***/ })
